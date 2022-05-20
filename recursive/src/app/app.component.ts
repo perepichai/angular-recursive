6c815d6d083661ec4f4514de6dec52d5c25fd83e
perepichai <o.perepichai@gmail.com>
2022-05-20 19:17:32 +0300

    recursive

--- recursive/src/app/app.component.ts
+++ recursive/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,197 +7,220 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'recursive';
-  ngOnInit(): void {
-    const arr = [
-      {
-        id: 1,
-        name: 'test1',
-        children: [
-          {
-            id: 2,
-            name: 'test2',
-            children: [
-              {
-                id: 3,
-                name: 'test3',
-                children: [
-                  {
-                    id: 4,
-                    name: 'test4',
-                    children: []
-                  },
-                  {
-                    id: 4,
-                    name: 'test4',
-                    children: [
-                      {
-                        id: 1,
-                        name: 'test1',
-                        children: [
-                          {
-                            id: 2,
-                            name: 'test2',
-                            children: [
-                              {
-                                id: 3,
-                                name: 'test3',
-                                children: [
-                                  {
-                                    id: 4,
-                                    name: 'test4',
-                                    children: []
-                                  }
-                                ]
-                              }
-                            ]
-                          },
-                          {
-                            id: 4,
-                            name: 'test4',
-                            children: [
-                              {
-                                id: 1,
-                                name: 'test1',
-                                children: [
-                                  {
-                                    id: 2,
-                                    name: 'test2',
-                                    children: [
-                                      {
-                                        id: 3,
-                                        name: 'test3',
-                                        children: [
-                                          {
-                                            id: 4,
-                                            name: 'test4',
-                                            children: []
-                                          }
-                                        ]
-                                      }
-                                    ]
-                                  }
-                                ]
-                              }
-                            ]
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      },
-      {
-        id: 1,
-        name: 'test1',
-        children: [
-          {
-            id: 2,
-            name: 'test2',
-            children: [
-              {
-                id: 3,
-                name: 'test3',
-                children: [
-                  {
-                    id: 4,
-                    name: 'test4',
-                    children: [
-                      {
-                        id: 1,
-                        name: 'test1',
-                        children: [
-                          {
-                            id: 2,
-                            name: 'test2',
-                            children: [
-                              {
-                                id: 3,
-                                name: 'test3',
-                                children: [
-                                  {
-                                    id: 4,
-                                    name: 'test4',
-                                    children: []
-                                  }
-                                ]
-                              }
-                            ]
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  {
-                    id: 4,
-                    name: 'test4',
-                    children: [
-                      {
-                        id: 1,
-                        name: 'test1',
-                        children: [
-                          {
-                            id: 2,
-                            name: 'test2',
-                            children: [
-                              {
-                                id: 3,
-                                name: 'test3',
-                                children: [
-                                  {
-                                    id: 4,
-                                    name: 'test4',
-                                    children: []
-                                  }
-                                ]
-                              }
-                            ]
-                          },
-                          {
-                            id: 4,
-                            name: 'test4',
-                            children: [
-                              {
-                                id: 1,
-                                name: 'test1',
-                                children: [
-                                  {
-                                    id: 2,
-                                    name: 'test2',
-                                    children: [
-                                      {
-                                        id: 3,
-                                        name: 'test3',
-                                        children: [
-                                          {
-                                            id: 4,
-                                            name: 'test4',
-                                            children: []
-                                          }
-                                        ]
-                                      }
-                                    ]
-                                  }
-                                ]
-                              }
-                            ]
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ];
-  }
-  
+  arr = [
+    {
+      id: 1,
+      name: 'test1',
+      children: [
+        {
+          id: 2,
+          name: 'test2',
+          children: [
+            {
+              id: 3,
+              name: 'test3',
+              children: [
+                {
+                  id: 4,
+                  name: 'test4',
+                  children: []
+                },
+                {
+                  id: 4,
+                  name: 'test4',
+                  children: [
+                    {
+                      id: 1,
+                      name: 'test1',
+                      children: [
+                        {
+                          id: 2,
+                          name: 'test2',
+                          children: [
+                            {
+                              id: 3,
+                              name: 'test3',
+                              children: [
+                                {
+                                  id: 4,
+                                  name: 'test4',
+                                  children: []
+                                }
+                              ]
+                            }
+                          ]
+                        },
+                        {
+                          id: 4,
+                          name: 'test4',
+                          children: [
+                            {
+                              id: 1,
+                              name: 'test1',
+                              children: [
+                                {
+                                  id: 2,
+                                  name: 'test2',
+                                  children: [
+                                    {
+                                      id: 3,
+                                      name: 'test3',
+                                      children: [
+                                        {
+                                          id: 4,
+                                          name: 'test4',
+                                          children: []
+                                        }
+                                      ]
+                                    }
+                                  ]
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: 'test1',
+      children: [
+        {
+          id: 2,
+          name: 'test2',
+          children: [
+            {
+              id: 3,
+              name: 'test3',
+              children: [
+                {
+                  id: 4,
+                  name: 'test4',
+                  children: [
+                    {
+                      id: 1,
+                      name: 'test1',
+                      children: [
+                        {
+                          id: 2,
+                          name: 'test2',
+                          children: [
+                            {
+                              id: 3,
+                              name: 'test3',
+                              children: [
+                                {
+                                  id: 4,
+                                  name: 'test4',
+                                  children: []
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  id: 4,
+                  name: 'test4',
+                  children: [
+                    {
+                      id: 1,
+                      name: 'test1',
+                      children: [
+                        {
+                          id: 2,
+                          name: 'test2',
+                          children: [
+                            {
+                              id: 3,
+                              name: 'test3',
+                              children: [
+                                {
+                                  id: 4,
+                                  name: 'test4',
+                                  children: []
+                                }
+                              ]
+                            }
+                          ]
+                        },
+                        {
+                          id: 4,
+                          name: 'test4',
+                          children: [
+                            {
+                              id: 1,
+                              name: 'test1',
+                              children: [
+                                {
+                                  id: 2,
+                                  name: 'test2',
+                                  children: [
+                                    {
+                                      id: 3,
+                                      name: 'test3',
+                                      children: [
+                                        {
+                                          id: 4,
+                                          name: 'test4',
+                                          children: []
+                                        }
+                                      ]
+                                    }
+                                  ]
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
 
+  constructor (
+    private renderer: Renderer2,
+    private el: ElementRef
+    ) {}
+
+  ngOnInit(): void {
+    this.walkThroughArray(this.arr);
+  }
+
+  walkThroughArray(arr: string | any[]) {
+    const ul = this.renderer.createElement('ul');
+
+    for (let i=0; i<arr.length; i++) {
+      const li = this.renderer.createElement('li');
+      const text = this.renderer.createText(`ID: ${arr[i].id}; Name: ${arr[i].name}`);
+      this.renderer.appendChild(li, text);
+
+      if (arr[i].children.length) {
+        this.renderer.appendChild(li, this.walkThroughArray(arr[i].children));        
+      }
+      this.renderer.appendChild(ul, li);
+    }
+    this.renderer.appendChild(this.el.nativeElement, ul);
+    return ul;
+}
 
 }
+
